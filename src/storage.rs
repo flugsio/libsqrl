877a7db70e484752f437e1a43263eade6f605765
Jimmie Elvenmark <flugsio@gmail.com>
2021-05-26 18:08:18 +0200

    wip, testing decryption

--- src/storage.rs
+++ src/storage.rs
@@ -232,6 +232,74 @@ impl S4 {
             (blocks)
         )
     }
+
+    // TODO: remove this
+    pub fn enscrypted_password(&self, password: &[u8]) -> Vec<u8> {
+        let mut enscrypted_password = vec![0u8, 32];
+        for block in &self.blocks {
+            // dbg!(block.clone());
+            match block {
+                //Block::BlockType1 { scrypt_random_salt, scrypt_log_n_factor, scrypt_iteration_count, aes_gcm_iv, encrypted_identity_master_key, encrypted_identity_lock_key, verification_tag, .. } => {
+                Block::BlockType1 { 
+                length, block_type, pt_length, aes_gcm_iv,
+                scrypt_random_salt, scrypt_log_n_factor, scrypt_iteration_count,
+                option_flags, hint_length, pw_verify_sec, idle_timeout_min,
+                encrypted_identity_master_key, encrypted_identity_lock_key,
+                verification_tag } => {
+                    let params = scrypt::Params::new(*scrypt_log_n_factor, 256, 1).unwrap();
+                    let mut output: Vec<u8> = vec![0u8; 32];
+                    let mut result;
+                    scrypt::scrypt(password, scrypt_random_salt, &params, &mut output).unwrap();
+                    result = output.clone();
+
+                    for _n in 1..*scrypt_iteration_count {
+                        // println!("{:02x?}", &result);
+                        scrypt::scrypt(password, &output.clone(), &params, &mut output).unwrap();
+                        result.iter_mut().zip(output.iter()).for_each(|(x1, x2)| *x1 ^= *x2);
+                    }
+                    // println!("{:02x?}", &result);
+                    const BINARY: [u8; 356] =
+                        [115, 113, 114, 108, 100, 97, 116, 97, 125, 0, 1, 0, 45, 0, 192, 52, 118, 104, 170, 33, 53,
+                        69, 178, 164, 139, 254, 99, 164, 222, 81, 102, 228, 163, 246, 171, 112, 252, 12, 7, 214, 165,
+                        164, 9, 4, 0, 0, 0, 241, 0, 4, 1, 15, 0, 238, 224, 209, 164, 16, 241, 168, 150, 113, 193, 73,
+                        1, 227, 47, 126, 167, 149, 214, 188, 6, 224, 84, 194, 180, 218, 91, 231, 72, 15, 254, 16, 17,
+                        227, 45, 170, 227, 160, 118, 29, 111, 229, 4, 85, 109, 171, 11, 140, 246, 81, 28, 142, 115,
+                        26, 66, 121, 5, 223, 26, 150, 80, 202, 230, 119, 117, 33, 162, 184, 129, 72, 213, 226, 197,
+                        95, 236, 103, 177, 27, 17, 5, 219, 73, 0, 2, 0, 162, 35, 43, 247, 123, 141, 243, 41, 97, 56,
+                        124, 240, 148, 249, 159, 84, 9, 20, 0, 0, 0, 78, 119, 187, 192, 235, 17, 141, 74, 53, 3, 204,
+                        108, 237, 94, 10, 218, 64, 233, 116, 170, 169, 30, 201, 135, 102, 147, 126, 233, 236, 142,
+                        112, 183, 195, 252, 107, 165, 226, 244, 114, 172, 192, 182, 166, 126, 212, 5, 165, 125, 150,
+                        0, 3, 0, 4, 0, 4, 18, 28, 173, 79, 150, 69, 44, 134, 241, 42, 53, 224, 139, 137, 185, 137,
+                        129, 11, 239, 121, 65, 107, 61, 184, 56, 108, 219, 164, 227, 177, 64, 143, 151, 84, 129, 166,
+                        191, 125, 10, 3, 252, 163, 34, 215, 182, 119, 140, 49, 126, 213, 240, 170, 162, 166, 55, 25,
+                        28, 49, 105, 111, 131, 205, 139, 68, 32, 228, 65, 216, 11, 17, 37, 72, 25, 90, 248, 153, 123,
+                        198, 167, 108, 181, 15, 229, 210, 173, 214, 189, 11, 200, 58, 83, 9, 55, 13, 155, 51, 239,
+                        125, 186, 188, 203, 238, 201, 238, 189, 206, 31, 179, 229, 40, 234, 70, 172, 0, 44, 216, 44,
+                        53, 24, 86, 247, 151, 168, 67, 60, 54, 185, 128, 50, 172, 247, 64, 6, 137, 124, 32, 169, 64,
+                        251, 255, 186, 140, 4];
+                    enscrypted_password = result;
+                    sodiumoxide::init();
+                    use sodiumoxide::crypto::aead::aes256gcm;
+                    use std::convert::TryInto;
+                    let aes = aes256gcm::Aes256Gcm::new().unwrap();
+                    let nonce = aes256gcm::Nonce(aes_gcm_iv.as_slice().try_into().expect("nonce slice with incorrect length"));
+                    let key = aes256gcm::Key(enscrypted_password.as_slice().try_into().expect("key slice with incorrect length"));
+                    let tag = aes256gcm::Tag(verification_tag.as_slice().try_into().expect("tag slice with incorrect length"));
+                    let data: Vec<u8> = BINARY[8..8+2+2+2+12+16+1+4+2+1+1+2].to_vec();
+                    let mut decrypted: Vec<u8> = BINARY[8+2+2+2+12+16+1+4+2+1+1+2..8+2+2+2+12+16+1+4+2+1+1+2+32+32].to_vec();
+                    println!("{}, {}", data.len(), decrypted.len());
+
+                    //let mut decrypted = encrypted_identity_master_key.clone();
+                    //decrypted.append(&mut encrypted_identity_lock_key.clone());
+                    aes.open_detached(&mut decrypted, Some(&data), &tag, &nonce, &key).unwrap();
+                    println!("{:?}", decrypted);
+                },
+                _ => ()
+            };
+        };
+
+        enscrypted_password
+    }
 }
 
 #[cfg(test)]
